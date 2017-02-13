cf38198572fb61a01645a874cc10aa799381f109
Cuxi <marferyan@gmail.com>
2017-02-13 08:10:46 +0100

    Add files via upload

--- dos/src/main/java/aula114/springmvc/controller/EmployeeController.java
+++ dos/src/main/java/aula114/springmvc/controller/EmployeeController.java
@@ -19,12 +19,7 @@ public class EmployeeController {
 
   @Autowired
   private EmployeeService employeeService;
-/*
-  @RequestMapping("/show/{clave}")
-  public ... {
-    //  
-  }
-  */
+
   @RequestMapping("/show/{clave}")
   public String show(Model model, @PathVariable String clave){
 	model.addAttribute("aContact", employeeService.show(clave));
@@ -34,9 +29,51 @@ public class EmployeeController {
    
   @RequestMapping("/employee")
   public String consulta(Model model) {
-    model.addAttribute("contact", employeeService.listIdEmployee());
+    	model.addAttribute("contact", employeeService.listIdEmployee());
     
-    return "consulta";
+    	return "consulta";
+  }
+
+  @RequestMapping("/delete")
+  public String delete (Model model, @RequestParam(value = "clave", required = false) String id){
+
+	int c=employeeService.delete(id);
+	Contact contact = new Contact();
+
+	model.addAttribute("filas",c);
+
+	return "deleteOK";
+  }
+
+  @RequestMapping("/insert")
+  public String insertar(Model model,@RequestParam(value="name") String name,@RequestParam(value="address") String address,@RequestParam(value="email") String email,@RequestParam(value="telephone") String telephone){
+	Contact contact=new Contact();
+	contact.setName(name);
+	contact.setAddress(address);
+	contact.setEmail(email);
+	contact.setTelephone(telephone);
+
+	int c = employeeService.insert(contact);
+	model.addAttribute("resultado", c);
+
+	return "insertOK";
+	
+  }
+
+
+  @RequestMapping("/editar/")
+  public String editar (Model model,@RequestParam(value="id") String id,@RequestParam(value="name", required=false) String name,@RequestParam(value="address", required=false) String address,@RequestParam(value="email", required=false) String email,@RequestParam(value="telephone", required=false) String telephone){
+
+	Contact contact=new Contact();
+	if(name!=null){contact.setName(name);}
+	if(address!=null){contact.setAddress(address);}
+	if(email!=null){contact.setEmail(email);}
+	if(telephone!=null){contact.setTelephone(telephone);}
+
+	int c = employeeService.insert(contact);
+	model.addAttribute("resultado", c);
+
+	return "updateOK";
   }
 
 }
